1f699589584572ec7e1e829a3e5481a8266cc031
Nick Roberts <nick@greektrack.com>
2025-06-17 16:35:22 -0600

    Rejiggering.

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,24 @@
 FROM nginx:1.21.5-alpine
 
-# install dig
-RUN apk add bind-tools
+# Installing bash
+RUN apk add --no-cache bash
+RUN sed -i 's/bin\/ash/bin\/bash/g' /etc/passwd
+
+# Installing other CLI tools
+RUN apk add --no-cache \
+    bind-tools \
+    curl \
+    git \
+    vim \
+    zip \
+    unzip
 
 # remove default NGINX confg
-RUN rm -Rf /etc/nginx/conf.d/default.conf /usr/share/nginx/html
+RUN rm -Rf /etc/nginx/conf.d/*.conf /usr/share/nginx/html
 
 # copy custom NGINX configuration files
-COPY nginx.conf.template /etc/nginx/nginx.conf.template
-COPY http-server.conf /etc/nginx/conf.d/http-server.conf
-COPY routes.conf.template /etc/nginx/routes.conf.template
+COPY docker/nginx/nginx.conf /etc/nginx/nginx.conf
+COPY docker/nginx/conf.d/default.conf /etc/nginx/conf.d/default.conf
 
 # copy NGINX commands to run
 COPY run.sh /etc/nginx/run.sh
--- run.sh
+++ run.sh
@@ -2,47 +2,45 @@
 
 set -e
 
-if [ -z "$KUBERNETES_DNS_SERVICE_IP" ]; then
-  nameserver=$(grep nameserver /etc/resolv.conf | awk '{print $2}' | head -1)
-  export KUBERNETES_DNS_SERVICE_IP=$nameserver
-fi
+#if [ -z "$KUBERNETES_DNS_SERVICE_IP" ]; then
+#  nameserver=$(grep nameserver /etc/resolv.conf | awk '{print $2}' | head -1)
+#  export KUBERNETES_DNS_SERVICE_IP=$nameserver
+#fi
 
-envsubst '$KUBERNETES_DNS_SERVICE_IP' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+#envsubst '$KUBERNETES_DNS_SERVICE_IP' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
-cp /etc/nginx/routes.conf.template /etc/nginx/routes.conf
+#fqdn=$(grep search /etc/resolv.conf | awk '{print $2}' | head -1)
 
-fqdn=$(grep search /etc/resolv.conf | awk '{print $2}' | head -1)
+#if [ -z "$fqdn" ]; then
+#  fqdn='local'
+#fi
 
-if [ -z "$fqdn" ]; then
-  fqdn='local'
-fi
+#for var in $(env)
+#do
+#  ENV_VAR_KEY="\$$(echo "$var" | cut -d= -f1)"
+#  ENV_VAR_VALUE=$(echo "$var" | cut -d= -f2-)
 
-for var in $(env)
-do
-  ENV_VAR_KEY="\$$(echo "$var" | cut -d= -f1)"
-  ENV_VAR_VALUE=$(echo "$var" | cut -d= -f2-)
-
-  case "$ENV_VAR_VALUE" in
-  *.*)
+#  case "$ENV_VAR_VALUE" in
+#  *.*)
     # there is a domain - so do not concatenate with fqdn
-    CMD="sed -i 's/$ENV_VAR_KEY/$(echo "$ENV_VAR_VALUE" | tr "/" "\/")/g' /etc/nginx/routes.conf"
-    ;;
-  *)
-    local_port="$(dig +noall +answer +time=3 +tries=1 srv \*._tcp."$ENV_VAR_VALUE"."$fqdn" | awk '{print $7}')"
+#    CMD="sed -i 's/$ENV_VAR_KEY/$(echo "$ENV_VAR_VALUE" | tr "/" "\/")/g' /etc/nginx/routes.conf"
+#    ;;
+#  *)
+#    local_port="$(dig +noall +answer +time=3 +tries=1 srv \*._tcp."$ENV_VAR_VALUE"."$fqdn" | awk '{print $7}')"
     ## TODO check local_port is a valid port number
-    CMD="sed -i 's/$ENV_VAR_KEY/$(echo "$ENV_VAR_VALUE" | tr "/" "\/").$fqdn:$local_port/g' /etc/nginx/routes.conf"
-    ;;
-  esac
+#    CMD="sed -i 's/$ENV_VAR_KEY/$(echo "$ENV_VAR_VALUE" | tr "/" "\/").$fqdn:$local_port/g' /etc/nginx/routes.conf"
+#    ;;
+#  esac
 
-  eval "$CMD"
-done
+#  eval "$CMD"
+#done
 
-printf "nameserver: %s\n" "$KUBERNETES_DNS_SERVICE_IP"
-printf "search FQDN: %s\n\n" "$fqdn"
+#printf "nameserver: %s\n" "$KUBERNETES_DNS_SERVICE_IP"
+#printf "search FQDN: %s\n\n" "$fqdn"
 
-printf "--------- GENERATED ROUTES.CONF ------------\n"
-cat /etc/nginx/routes.conf
-printf "--------- EOF GENERATED ROUTES.CONF ---------\n"
+#printf "--------- GENERATED ROUTES.CONF ------------\n"
+#cat /etc/nginx/routes.conf
+#printf "--------- EOF GENERATED ROUTES.CONF ---------\n"
 
 printf "> nginx.conf and routes.conf configuration files loaded!\n\n"
 
